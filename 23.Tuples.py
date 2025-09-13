b0cb5541b42194f8afe1b33900b45ba1d91b54eb
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:39:52 +0530

    Add files via upload

---
+++ 23.Tuples.py
@@ -0,0 +1,16 @@
+# In this tutorial, I am going to talk to
+# you guys about another important structure.
+# Tuples are similar to lists, so we can use them to store a list of items.
+# But unlike lists we cannot modify them, we cannot add new items,
+# we cannot remove existing items.
+# Tuples are immutable. We cannot mutable or change them.
+# 😃Let me show you.So I am going to start by defining a list of numbers.
+# We can count and index. (2)
+# 1.We use count() to count the number of occurrences in an item,
+# and 2.index(), to find the index of the first occurrence,
+# 🙃We can only get information about a tuple, we cannot change it.
+
+
+numbers = (1, 2, 3)
+#numbers[0] = 10
+print(numbers[0]) # 1
