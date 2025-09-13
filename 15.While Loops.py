a610a9ec970ba8f5aaedbf56e4a2237433f72d52
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:36:40 +0530

    Add files via upload

---
+++ 15.While Loops.py
@@ -0,0 +1,13 @@
+"""
+i = 1   # i is initialized to 1
+while i <= 5:  # loop runs as long as i is less than or equal to 5
+    print(i)   # prints the current value of i
+    i = i + 1  # increments i by 1 after each loop
+print("Done")  # prints "Done" after the loop ends
+"""
+
+i = 1
+while i <= 5:
+    print("*" * i)
+    i = i + 1
+print("Done")
