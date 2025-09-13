b3cff996defe0bbf752ffa6e7d1df00120e5f26f
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:32:21 +0530

    Add files via upload

---
+++ 03.Reciving_input.py
@@ -0,0 +1,7 @@
+name = input('What is your name? :')
+print('Hi '+ name)
+print('💡💡💡💡💡💡💡💡💡💡💡💡💡💡💡💡')
+#Exercise
+name = input('What is your name? ')
+favourite_color = input('What is your favourite color? ')
+print(name + ' likes ' + favourite_color)
