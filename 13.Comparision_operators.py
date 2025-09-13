7221bb8e1f39deba2c9c92705309437aa92c1cf8
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:36:07 +0530

    Add files via upload

---
+++ 13.Comparision_operators.py
@@ -0,0 +1,20 @@
+#Comparision Operator
+"""temperature = float(input("Enter the temperature: "))  # Taking user input
+
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("It's a cold day")
+else:
+    print("It's neither hot nor cold")"""
+
+name = "John Smith"
+
+if len(name) < 3:
+    print("Name must be at least 3 characters.")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 characters")
+else:
+    print("Name looks good!")
+
+#Note:(len(name)) => Number of Characters
