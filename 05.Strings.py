3188d627c8b0477cfd6b3e7bb556df53b197c350
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:33:43 +0530

    Add files via upload

---
+++ 05.Strings.py
@@ -0,0 +1,28 @@
+"""course = 'Python for "Beginners"'
+print(course)"""
+
+"""course = '''
+Hi Dinu,
+
+Here is our first email to you.
+
+Thank you,
+The support team
+
+'''
+print(course)"""
+
+"""course = 'Python for Beginners'
+print(course[-3])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+print(course[:]) #copy
+print('📌📌📌📌📌📌📌📌📌📌')"""
+
+course = 'Python for Beginners'
+another =course[:]
+print(another)
+
+name = 'Jennifer'
+print(name[1:-1])
